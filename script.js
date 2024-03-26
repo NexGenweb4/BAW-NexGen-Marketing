ac35d0f52f4b8595f2d9c79ea3f5687b5eaf6355
ahmad <ar7864245@gmail.comm>
2024-03-26 21:11:59 +0500

    Version 1.0.0

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerIcon = document.querySelector('.ham-icon');
+const menu = document.querySelector('.menu');
+
+hamburgerIcon.addEventListener('click', () => {
+  menu.classList.toggle('show-menu');
+});
